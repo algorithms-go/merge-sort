b77b668b99c806dc8565eccb0054d07b9665a37b
steevehook <steevehook@gmail.com>
2021-03-31 05:44:35 +0300

    finish merge sort

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 )
 
 func main() {
-	A := []int{2,4,5,7,1,2,3,6}
-	fmt.Println(sort.MergeSort(A, 0, 3, 7))
+	//A := []int{2, 4, 5, 7, 1, 2, 3, 6}
+	//A := []int{5, 2, 4, 7, 1, 3, 2, 6}
+	A := []int{8, 7, 6, 5, 4, 3, 2, 1}
+	fmt.Println(sort.MergeSort(A))
+	// call merge on multiple sets to prove it works
+	// explain merge in diagrams & animations
+	// explain recursive call in diagrams & animations
 }
